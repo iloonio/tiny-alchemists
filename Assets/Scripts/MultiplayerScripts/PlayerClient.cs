30a5fa936e7360636ddfcbc9ec93293e3b0560eb
iloonio <decadentlithium@gmail.com>
2026-04-08 23:09:58 +0200

    added basic multiplayer, but it needs to be developed further and integrate with unity sessions...

--- Assets/Scripts/MultiplayerScripts/PlayerClient.cs
+++ Assets/Scripts/MultiplayerScripts/PlayerClient.cs
@@ -1,16 +1,36 @@
+using Unity.Netcode;
+using Unity.Services.Matchmaker.Models;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerClient : MonoBehaviour
+public class PlayerClient : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private PlayerMovementFPS playerMovementFPS;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private AudioListener playerAudioListener;
+
+    private void Awake()
     {
-        
+        playerMovementFPS.enabled = false;
+        playerCamera.enabled = false;
+        playerAudioListener.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnNetworkSpawn()
     {
-        
+        if (IsOwner)
+        {
+            // This is the local player, so we can enable the PlayerController script.
+            playerMovementFPS.enabled = true;
+            playerCamera.enabled = true;
+            playerAudioListener.enabled = true;
+        }
+        else
+        {
+            // This is a remote player, so we can disable the PlayerController script.
+            playerMovementFPS.enabled = false;
+            playerCamera.enabled = false;
+            playerAudioListener.enabled = false;
+        }
     }
 }
