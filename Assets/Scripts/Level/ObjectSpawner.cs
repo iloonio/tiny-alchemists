4f4c922317c9cb661d1316991df8b53cf8be26d9
eryngia <barnabas.tanczos@gmail.com>
2026-05-27 01:37:08 +0200

    Add object spawners
    Objects are periodically spawned instead of starting in scene (no new objects spawn until the previous one is removed)

--- Assets/Scripts/Level/ObjectSpawner.cs
+++ Assets/Scripts/Level/ObjectSpawner.cs
@@ -1,30 +1,53 @@
 using UnityEngine;
 using Unity.Netcode;
+using System.Collections;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(BoxCollider))]
 public class ObjectSpawner : NetworkBehaviour
 {
     
     [SerializeField] private NetworkObject _objectPrefab;
+    [SerializeField] private float _secondsBetweenSpawns = 30f;
+    private HashSet<Collider> _inside = new();
 
     public override void OnNetworkSpawn()
     {
         if (IsServer)
         {
-            NetworkObject networkObject = Instantiate(_objectPrefab, transform.position, transform.rotation);
-            networkObject.Spawn();
+            StartCoroutine(SpawnObjects());
         }
+    }
+
+    private IEnumerator SpawnObjects()
+    {
+        while (true)
+        {    
+            if (_inside.Count == 0) {
+                NetworkObject networkObject = Instantiate(_objectPrefab, transform.position, transform.rotation);
+                networkObject.Spawn();
+            }
+
+            yield return new WaitForSeconds(_secondsBetweenSpawns);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        _inside.Add(collider);
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        _inside.Remove(collider);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
-        if (_objectPrefab != null )
+        if (_objectPrefab != null && _objectPrefab.TryGetComponent(out Ingredient ingredient))
         {
-            Renderer renderer = _objectPrefab.GetComponentInChildren<Renderer>();
-            if (renderer != null)
-            {
-                Gizmos.color = renderer.sharedMaterial.color;
-            }
+            Gizmos.color = ingredient.Type.Color;
         }
         Gizmos.DrawSphere(transform.position, 0.1f);
     }
